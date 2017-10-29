16000d9c18d709dbb8b3e936a8e01c0965dd3777
renan028 <renan028@gmail.com>
2017-10-29 16:49:30 -0200

    FIX input planta

--- matlab/diophantina.m
+++ matlab/diophantina.m
@@ -1,21 +1,35 @@
+clear all
+clc
 s = tf('s');
 
 % ------------- Input Parameters -------------
 bp1 = 1;
-bp2 = 1;
-ap1 = 2;
+bp2 = 2;
+ap1 = -2;
 ap0 = 1;
+
+N = (bp1*s+bp2);
+D = (s^2 + ap1*s +ap0);
+y = N/D; % Planta
 
 % --------------------------------------------
 
-y = (b1*s+b2)/(s^2 + a1*s +a0); % Planta
-[num,den] = tfdata(y);
-n = length(nonzeros(den{1}));
-n_star = length(nonzeros(num{1})) - n;
+[num,den] = tfdata(y,'v');
+index = find(den);
+index = index(1);
+n = length(den(index:end)) - 1;
 
-ym = 1/(s+1)^n_star;
+index = find(num);
+index = index(1);
+n_star = n - (length(num(index:end)) - 1);
+
+Nm = (s+1)^(n-n_star);
+Dm = (s+2)^n;
+ym = Nm/Dm; %Model
 
 A0 = (s+1)^(n_star-1);
-
-
-
+L = Nm*A0;
+H = (s+1)^(n_star-1);
+DmA0 = Dm*A0;
+HD = H*D;
+G = DmA0-HD;
