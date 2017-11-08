703171def114b378f63e8ec94a49c76d858bdc8c
Guilherme Carvalho <guilherme.ps.carvalho@gmail.com>
2017-11-08 13:23:34 -0200

    CHANGE matlab listing style. Also added some text to teoria and reviewed implementacao. Signed-off-by: Guilherme Carvalho <guilherme.ps.carvalho@gmail.com>

--- matlab/find2DOFparameters.m
+++ matlab/find2DOFparameters.m
@@ -1,42 +1,45 @@
-function [theta_1, theta_n, theta_2n, theta_2, L] = find2DOFparameters(y,ym,A0)
+function [theta_1, theta_n, theta_2n, theta_2, L] = find2DOFparameters(P,Pm,A0)
 s = tf('s');
 
+    %Extract transfer function properties
     function [n, n_star, num, den] = tfprop(y)
         [num,den] = tfdata(y,'v');
         num = num(find(num,1):end);
         den = den(find(den,1):end);
-        n = length(den) - 1; % calculating n
-        n_star = n - (length(num) - 1); % calculating n_star
+        n = length(den) - 1;
+        n_star = n - (length(num) - 1);
     end 
 
+    %Customized convolution (removes first zero entries)
     function A = myconv(n,m)
         A = conv(n,m);
         A = A(find(A,1):end);
     end
 
-
-[n, n_star, numy, deny] = tfprop(y);
-kp = numy(1); % Calculating kp
+%Get plant TF properties
+[n, n_star, numy, deny] = tfprop(P);
+kp = numy(1);
 numy = numy/kp;
 
-[m, ~, ~,~] = tfprop(ym);
-
+%Get model TF properties and add equal poles and zeros to model TF if n > m
+[m, ~, ~,~] = tfprop(Pm);
 if n>m
-    ym = ym*((s+1)^(n-m))/((s+1)^(n-m));
+    Pm = Pm*((s+1)^(n-m))/((s+1)^(n-m));
 end
-[~, ~, numym, denym] = tfprop(ym);
-km = numym(1); % Calculating km
+[~, ~, numym, denym] = tfprop(Pm);
+km = numym(1);
 numym = numym/km;
 
+%Get A0 polynomial
 [A0,~] = tfdata(A0,'v');
 
-L = myconv(numym,A0); % Creating Lambda
-
+%Finding Lambda and Dm*A0
+L = myconv(numym,A0);
 DmA0 = myconv(denym, A0);
 
+%Solve Diophantina Equation to find G and H
 degH = n_star-1; 
 degG = n-1;
-
 A = convmtx(deny',degH+1);
 S = zeros(n_star + n, degG+1);
 S(n_star+1:end,1:end) = -kp*eye(degG+1);
@@ -45,11 +48,13 @@ HG = B\DmA0';
 H = HG(1:degH+1);
 G = HG(degH+2:end);
 
-theta_n = G(1);
-theta_2 = G(2:end)' - theta_n*L(2:end);
-
+%Find F polynomial
 F = L - myconv(numy,H');
 
+%Find thetas
 theta_1 = F(2:end);
+theta_n = G(1);
+theta_2 = G(2:end)' - theta_n*L(2:end);
 theta_2n = km/kp;
+
 end
